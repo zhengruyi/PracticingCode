fb49906bad0afb4817a3901be0d311c431444b0e
Ruyi ZHENG <standonthpeak@gmail.com>
2020-10-10 22:56:21 +0200

    添加注释

--- NowCoder/src/Others/Q1吃葡萄/Main.java
+++ NowCoder/src/Others/Q1吃葡萄/Main.java
@@ -16,18 +16,22 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         sc.nextInt();
+        //nextLong也可以用来读取int类型
         while(sc.hasNextLong()){
             long[] nums = new long[3];
             nums[0] = sc.nextLong();
             nums[1] = sc.nextLong();
             nums[2] = sc.nextLong();
             Arrays.sort(nums);
+            //当三个数字可以构成三角形时
             long x = (nums[0] + nums[1] + nums[2] + 2) / 3;
             if(nums[0]+nums[1] > nums[2]){
                 System.out.println(x);
+            //当三个数字可以构成一个四边形时
             }else if(nums[2] <= 2*(nums[0]+nums[1])){
                 System.out.println(x);
             }else{
+                //某一条边过长，此时最少要吃掉最大数字一半的葡萄
                 System.out.println((nums[2]+1)/2);
             }
         }
