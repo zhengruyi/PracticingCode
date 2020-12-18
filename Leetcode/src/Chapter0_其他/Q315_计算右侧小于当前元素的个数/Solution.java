38efa74f2c9d520044ec2816274989169ac9b665
Ruyi ZHENG <standonthpeak@gmail.com>
2020-12-19 00:41:21 +0100

    leetcode 315 and 剑指0ffer

--- Leetcode/src/Chapter0_其他/Q315_计算右侧小于当前元素的个数/Solution.java
+++ Leetcode/src/Chapter0_其他/Q315_计算右侧小于当前元素的个数/Solution.java
@@ -22,9 +22,11 @@ public class Solution {
         int[] temp = new int[len];
         //存放最终结果的数组
         int[] res = new int[len];
+        //初始化索引数组
         for(int i = 0; i< len;i++){
             indexes[i] = i;
         }
+        //进行归并排序来统计结果
         mergerAndCounter(nums,0,len-1,indexes,temp,res);
         for(int i : res){
             result.add(i);
@@ -35,33 +37,46 @@ public class Solution {
         if(left == right){
             return;
         }
+        //计算分割点
         int mid = left +(right - left)/2;
+        //归并排序左半部分
         mergerAndCounter(nums,left,mid,indexes,temp,res);
+        //归并排序右半部分
         mergerAndCounter(nums,mid+1,right,indexes,temp,res);
+        //优化，如果左半部分都小于右半部分，那么不需要合并，数组已经有序，也就不存在逆序
         if(nums[indexes[mid]] <= nums[indexes[mid+1]]){
             return;
         }
+        //合并两个有序链表
         mergerTwoSortedList(nums,left,mid,right,indexes,temp,res);
     }
     public void mergerTwoSortedList(int[] nums, int left,int mid, int right, int[] indexes, int[] temp, int[] res){
+        //因为indexes要存放排序后的结果
         for(int i = left; i<= right; i++){
             temp[i] = indexes[i];
         }
+        //确定两个有序数组的起始点
         int i = left;
         int j = mid+1;
         for(int k = left; k<= right; k++){
+            //左子数组已经存入完毕
             if(i > mid){
                 indexes[k] = temp[j];
                 j++;
+                //右子数组都存入完毕
             }else if(j > right){
                 indexes[k] = temp[i];
                 i++;
+                //此时对于每一个左子数组的元素来说，都比右子数组大，所以存在right-mid个元素，因为Mid是左子数组的最右一个元素
+                //所以这里不用加1
                 res[indexes[k]] += (right - mid);
             }else if(nums[temp[i]] <= nums[temp[j]]){
-                indexes[k] = temp[i];
+                //代表当前i小于对应的j，那么i一定大于mid+1..j-1之间的数字，所以可以加上j-mid-1
+                indexes[k] = temp[i] ;
                 i++;
                 res[indexes[k]] += (j - mid -1);
             }else{
+                //j小于对应的i,那么j往后退一步
                 indexes[k] = temp[j];
                 j++;
             }
---
+++ 剑指offer/src/Q51_数组中的逆序对/Solution.java
@@ -0,0 +1,59 @@
+package Q51_数组中的逆序对;
+
+/**
+ * @author Ruyi ZHENG
+ * @version 1.00
+ * @time 19/12/2020 00:19
+ **/
+
+public class Solution {
+    int count = 0;
+    public int reversePairs(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int[] temp = new int[nums.length];
+        //归并排序
+        mergerAndCounter(nums,0,nums.length-1,temp);
+        return count;
+    }
+    public void mergerAndCounter(int[] nums, int left, int right, int[] temp){
+        if(left == right){
+            return;
+        }
+        int mid = left+(right - left)/2;
+        mergerAndCounter(nums, left,mid,temp);
+        mergerAndCounter(nums,mid+1, right,temp);
+        //归并优化，如果两个子数组已经有序就不需要在合并，并且也不会存在逆序对
+        if(nums[mid] <= nums[mid+1]){
+            return;
+        }
+        //合并有序数组，同时计算逆序对数目
+        counter(nums,left,mid,right,temp);
+
+    }
+    public void counter(int[] nums, int left, int mid, int right, int[] temp){
+        for(int i = left; i <= right;i++){
+            temp[i] = nums[i];
+        }
+        int i = left;
+        int j = mid+1;
+        for(int k = left; k <= right;k++){
+            if(i > mid){
+                nums[k] = temp[j];
+                j++;
+            }else if(j > right){
+                nums[k] = temp[i];
+                i++;
+                count +=(right - mid);
+            }else if(temp[i] <= temp[j]){
+                nums[k] = temp[i];
+                i++;
+                count += (j-mid - 1);
+            }else{
+                nums[k] = temp[j];
+                j++;
+            }
+        }
+    }
+}
