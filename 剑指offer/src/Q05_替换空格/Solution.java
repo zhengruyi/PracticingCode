4be37542ada1d8372a63e9e58ae1f09bfecfaf50
Ruyi ZHENG <standonthpeak@gmail.com>
2021-03-31 15:10:01 +0200

    剑指offer 05

---
+++ 剑指offer/src/Q05_替换空格/Solution.java
@@ -0,0 +1,13 @@
+package Q05_替换空格;
+
+/**
+ * @author Ruyi ZHENG
+ * @version 1.00
+ * @time 31/03/2021 15:09
+ **/
+
+public class Solution {
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ","%20");
+    }
+}
